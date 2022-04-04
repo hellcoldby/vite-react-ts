6c7176ae17cc900c850814a617c7a45c9b0b1f33
coldbao <coldbao@gmail.com>
2022-04-05 02:40:50 +0800

    add demo

---
+++ jest/hook.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { render, waitFor } from "@testing-library/react";
+import { renderHook } from "@testing-library/react-hooks";
+
+test("Hooks 测试", () => {
+  const useCounter = () => {
+    const [count, setCount] = React.useState(0);
+    const increment = React.useCallback(() => setCount((x) => x + 1), []);
+    return { count, increment };
+  };
+  const { result } = renderHook(() => useCounter());
+  // result.current 包含hooks的返回值
+  expect(result.current.count).toBe(0);
+  // result.current.increment()的调用需要放在waitFor里
+  waitFor(() => result.current.increment());
+  expect(result.current.count).toBe(1);
+});
--- jest/test.test.js
+++ jest/test.test.js
@@ -1,15 +1,20 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+
+
+
+//常规测试
 test('2+2', ()=>{
     expect(2+2).toBe(4)
 });
 
-//测试button 点击
 
 
 
-import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+
+
 /**
- * @jest-environment jsdom
+ * react 测试button 点击
  */
 test("component", () => {
   const onClick = jest.fn(); // 测试函数
@@ -22,4 +27,37 @@ test("component", () => {
   fireEvent.click(btn); // 模拟点击事件
   expect(onClick).toBeCalled(); // 期望被调用
   expect(onClick).toBeCalledTimes(1); // 期望被调用一次
+});
+
+
+/**
+ * input 输入和输出
+ */
+test("输入框输入,校验值", () => {
+  const onChange = jest.fn();
+  const { getByTestId } = render(
+    <input data-testid="input" onChange={onChange} />
+  );
+  // 通过data-testid的方式来获取元素
+  const input = getByTestId("input");
+  // 模拟change事件，第二个参数模拟event的值
+  fireEvent.change(input, { target: { value: "test" } });
+  expect(onChange).toBeCalled();
+  expect(input).toHaveValue("test");
+});
+
+
+/**
+ * 测试元素是否被disable，是否包含某一类名
+ */
+test("测试元素是否disabled，是否包含某一类名", () => {
+  const { getByText } = render(
+    <button disabled className="button-disabled">
+      this is a button
+    </button>
+  );
+  // getByText从text来获取元素
+  const btn = getByText("this is a button");
+  expect(btn).toBeDisabled();
+  expect(btn).toHaveClass("button-disabled");
 });
